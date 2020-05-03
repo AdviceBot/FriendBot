c5e21cf83ea0ca6fd247d66c0a587366491c3491
Anna Szczepanska <anna.szczepanska2@ibm.com>
2020-05-03 05:11:14 +0200

    Add attachments handling and postaback message (tutorial example)

--- server-app/lib/messenger.js
+++ server-app/lib/messenger.js
@@ -5,15 +5,54 @@ function handleMessage(sender_psid, received_message) {
     let response;
     if (received_message.text) {
         response = {
-            "text": `You sent the message: "${received_message.text}". Now send me an image!`
+            "text": `You sent the message: "${received_message.text}"`
+        }
+    } else if (received_message.attachments) {
+        // Get the URL of the message attachment
+        let attachment_url = received_message.attachments[0].payload.url;
+        response = {
+            "attachment": {
+                "type": "template",
+                "payload": {
+                    "template_type": "generic",
+                    "elements": [{
+                        "title": "Is this the right picture?",
+                        "subtitle": "Tap a button to answer.",
+                        "image_url": attachment_url,
+                        "buttons": [
+                            {
+                                "type": "postback",
+                                "title": "Yes!",
+                                "payload": "yes",
+                            },
+                            {
+                                "type": "postback",
+                                "title": "No!",
+                                "payload": "no",
+                            }
+                        ],
+                    }]
+                }
+            }
         }
     }
+
+    // Send the response message
     callSendAPI(sender_psid, response);
 }
 
 // Handles messaging_postbacks events
 function handlePostback(sender_psid, received_postback) {
+    let response;
+    let payload = received_postback.payload;
+    // Set the response based on the postback payload
+    if (payload === 'yes') {
+        response = { "text": "Thanks!" }
+    } else if (payload === 'no') {
+        response = { "text": "Oops, try sending another image." }
+    }
 
+    callSendAPI(sender_psid, response);
 }
 
 // Sends response messages via the Send API
